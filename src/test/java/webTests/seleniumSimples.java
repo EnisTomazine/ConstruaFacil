9bce890ac4f933c61f658168767498cdf243666a
EnisTomazine <denistom2011@gmail.com>
2021-07-07 19:43:27 -0300

    Selenium puro

--- src/test/java/webTests/seleniumSimples.java
+++ src/test/java/webTests/seleniumSimples.java
@@ -22,7 +22,7 @@ public class seleniumSimples {
         System.setProperty("webdriver.chrome.driver","drivers/chrome/91/chromedriver.exe");
         //Instancia o objeto driver como um controlador do Chrome
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3000000, TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
 
         //8 - Realizar o teste
 
@@ -34,6 +34,35 @@ public class seleniumSimples {
         driver.findElement(By.id("btn_form_search")).click();                   //Clique na lupa
 
         assertEquals(driver.findElement(By.cssSelector("H3")).getText(),"Cursos › \"mantis\"");
+
+        //assertThat(driver.findElement(By.cssSelector("h3")).getText(), is("Cursos › \\\"mantis\\\""));
+
+        driver.findElement(By.cssSelector("span.comprar")).click();
+        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Mantis" );
+        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 49,99" );
+        driver.quit();
+
+
+    }
+
+    @Test
+    public void consultaCursoFundamentosDoJava(){
+        // A- Inicio
+        System.setProperty("webdriver.chrome.driver","drivers/chrome/91/chromedriver.exe");
+        //Instancia o objeto driver como um controlador do Chrome
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+
+        //8 - Realizar o teste
+
+        driver.get("https://www.iterasys.com.br");                              //Abre o site alvo informado
+        driver.findElement(By.id("searchtext")).click();                        //Clica no campo de pesquisa
+        driver.findElement(By.id("searchtext")).clear();                        //Limpa o campo de pesquisa
+        driver.findElement(By.id("searchtext")).sendKeys("introdução");  //Escreve "mantis" no campo
+
+        driver.findElement(By.id("btn_form_search")).click();                   //Clique na lupa
+
+        assertEquals(driver.findElement(By.cssSelector("H3")).getText(),"Cursos › \"introdução\"");
 
         //assertThat(driver.findElement(By.cssSelector("h3")).getText(), is("Cursos › \\\"mantis\\\""));
 
