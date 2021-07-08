e5ecb2fc2da03ca09a7b8ae18371092af579b34e
EnisTomazine <denistom2011@gmail.com>
2021-07-07 22:41:00 -0300

    Criação de script de testes do código do seleniumIDE

--- src/test/java/webTests/seleniumSimples.java
+++ src/test/java/webTests/seleniumSimples.java
@@ -1,14 +1,15 @@
+
 // 1- Pacote
-package webTests;
+        package webTests;
 // 2 - Bibliotecas
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
-
 import static org.testng.Assert.assertEquals;
 
 // 3 - Classes
@@ -16,15 +17,19 @@ public class seleniumSimples {
     //3.1 - Atributos
     WebDriver driver;
     //3.2 - Metodos e Funçoes
-    @Test
-    public void consultaCurso(){
+
+    @BeforeMethod
+    public void iniciar(){
         // A- Inicio
         System.setProperty("webdriver.chrome.driver","drivers/chrome/91/chromedriver.exe");
         //Instancia o objeto driver como um controlador do Chrome
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+    }
+    @Test(priority = 1)
+    public void consultaCurso(){
 
-        //8 - Realizar o teste
+        //B- Realizar o teste
 
         driver.get("https://www.iterasys.com.br");                              //Abre o site alvo informado
         driver.findElement(By.id("searchtext")).click();                        //Clica no campo de pesquisa
@@ -40,38 +45,39 @@ public class seleniumSimples {
         driver.findElement(By.cssSelector("span.comprar")).click();
         assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Mantis" );
         assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 49,99" );
-        driver.quit();
+
 
 
     }
 
-    @Test
-    public void consultaCursoFundamentosDoJava(){
-        // A- Inicio
-        System.setProperty("webdriver.chrome.driver","drivers/chrome/91/chromedriver.exe");
-        //Instancia o objeto driver como um controlador do Chrome
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
+    @Test(priority = 2)
+    public void consultaCursoCtfl(){
 
-        //8 - Realizar o teste
+        //B - Realizar o teste
 
         driver.get("https://www.iterasys.com.br");                              //Abre o site alvo informado
         driver.findElement(By.id("searchtext")).click();                        //Clica no campo de pesquisa
         driver.findElement(By.id("searchtext")).clear();                        //Limpa o campo de pesquisa
-        driver.findElement(By.id("searchtext")).sendKeys("introdução");  //Escreve "mantis" no campo
+        driver.findElement(By.id("searchtext")).sendKeys("preparatório ctfl");  //Escreve "mantis" no campo
 
         driver.findElement(By.id("btn_form_search")).click();                   //Clique na lupa
 
-        assertEquals(driver.findElement(By.cssSelector("H3")).getText(),"Cursos › \"introdução\"");
+        assertEquals(driver.findElement(By.cssSelector("H3")).getText(),"Cursos › \"preparatório ctfl\"");
 
         //assertThat(driver.findElement(By.cssSelector("h3")).getText(), is("Cursos › \\\"mantis\\\""));
 
         driver.findElement(By.cssSelector("span.comprar")).click();
-        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Mantis" );
-        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 49,99" );
-        driver.quit();
+        assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Preparatório CTFL" );
+        assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 169,00" );
 
 
+
+    }
+
+    @AfterMethod
+    public void finalizar(){
+        //Parte C - Encerramento
+        //driver.quit();
     }
 
 }
