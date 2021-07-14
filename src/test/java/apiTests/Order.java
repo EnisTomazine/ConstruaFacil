18ad68b144e9aaab70a34a171b82b43420688508
EnisTomazine <denistom2011@gmail.com>
2021-07-13 21:19:43 -0300

    teste de API Order

--- src/test/java/apiTests/Order.java
+++ src/test/java/apiTests/Order.java
@@ -1,5 +1,6 @@
 package apiTests;
 
+
 import org.hamcrest.text.StringContainsInOrder;
 import org.testng.annotations.Test;
 
@@ -11,73 +12,56 @@ import java.nio.file.Paths;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-public class Pet {
-    String uri = "https://petstore.swagger.io/v2/pet";
+public class Order {
+
+    String uri = "https://petstore.swagger.io/v2/store/order";
     int petId = 1123;
+
 
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
-    @Test(priority = 0)
-    public void incluirPet() throws IOException {
 
-        //Padrão
-        //Given = Dado
-        //.When = Quando
-        //.Then = Então
-        String jsonBody = lerJson("src/test/resources/data/pet.json");
+    @Test(priority = 0)
+    public void incluirOrder() throws IOException {
+
+        String jsonBody = lerJson("src/test/resources/data/order.json");
 
         given()                                             //Dado
                 .contentType("application/json")            //Tipo do conteúdo
                 .log().all()                                //registrar tudo do envio
                 .body(jsonBody)
         .when()                                                //Quando
-            .post(uri)   //Comando + endpoint
+                .post(uri)   //Comando + endpoint
         .then()                                                 //então
                 .log().all()                                        //Registrar tudo da volta
                 .statusCode(200)                                //Valida o Codigo do Estado
-                //.body("code", is(200))                 //Valida o Código de estado no Json
-                .body("id", is(petId))           //Valida o Id do Animal
-                .body("name" , is("Teste1"))    //Valida o nome do Animal
-                .body("category.name", is("dog")) //valida a categoria do Animal
-                //.body("tags.name", not(contains("animal")))
-                .body("tags.name[0]", stringContainsInOrder("vermi"))
+                .body("petId", is(petId))           //Valida o petId
+                .body("quantity", is(10))    //Valida a quantidade
+                //.body("shipDate", is("2021-07-13T21:39:40.937Z")) //valida a data
+                .body("status", is("placed"))
         ;
     }
 
-    @Test(priority = 1, dependsOnMethods = {"incluirPet"})
-    public void consultarPet(){
-        //String petId = "1123"; //Id do Animal
-
+    @Test(priority = 1, dependsOnMethods = {"incluirOrder"})
+    public void consultarOrder() {
         given()
                 .contentType("application/json")
                 .log().all()
         .when()
                 .get(uri + "/" + petId)
         .then()                                                 //então
-            .log().all()                                        //Registrar tudo da volta
-            .statusCode(200)
-                .body("name", is("Teste1"))
-                .body( "status", is("available"))
-        ;
-    }
-    @Test(priority = 2, dependsOnMethods = {"consultarPet"})
-    public void alterarPet() throws IOException {
-        String jsonBody = lerJson("src/test/resources/data/newpet.json");
-
-        given()
-                .contentType("application/json")
-                .log().all()
-                .body(jsonBody) //Json a ser transmitido para a alteração
-        .when()
-                .put(uri)
-        .then()
+                .log().all()                                        //Registrar tudo da volta
                 .statusCode(200)
-                .body("status", is("sold"))
+                .body("petId", is(petId))           //Valida o petId
+                .body("quantity", is(10))    //Valida a quantidade
+                //.body("shipDate", contains("2021-07-13T21:39:40.937Z")) //valida a data
+                .body("status", is("placed"))
         ;
     }
-    @Test(priority = 3, dependsOnMethods = {"alterarPet"})
-    public void excluirPet(){
+
+    @Test(priority = 2, dependsOnMethods = {"consultarOrder"})
+    public void excluirOrder() {
 
         given()
                 .contentType("application/json")
@@ -92,4 +76,6 @@ public class Pet {
                 .body("message", is(Integer.toString(petId)))
         ;
     }
+
+
 }
